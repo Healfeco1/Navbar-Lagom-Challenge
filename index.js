ff1600908ff39ec126ea81b4c28ea89730f000e8
Hector Alionzo Felix Covarrubias <healfeco@gmail.com>
2020-12-10 16:07:56 -0700

    Added functions to close menu navbar when is keyup esc or cliked in the page

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const menuIcon = document.querySelector("#menuToggle input");
-const navbar = document.getElementsByTagName('nav');
+const navbar = document.querySelector('.menu');
 const container = document.querySelector('body');
 
 menuIcon.addEventListener('click' , () => {
-  menuIcon.classList.toggle('change');
   container.classList.toggle('menu-open');
 });
+$(document).keyup(function(e) {
+  // escape key maps to keycode `27`
+  if (e.keyCode == 27 && menuIcon.checked == true) 
+  { 
+    closeNavbar();
+  }
+});
+
+$('body').click(function (e) { 
+  if(!navbar.contains(e.target) && !menuIcon.contains(e.target) && menuIcon.checked == true){
+    closeNavbar();
+  }
+});
+
+// Close the Menu Navbar
+function closeNavbar (){
+  container.classList.toggle('menu-open');
+  menuIcon.checked = false;
+}
